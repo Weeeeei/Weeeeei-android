0723ed27df025b00ea2719fcc2748563ed2f08c4
Takatoshi Maeda <takatoshi-maeda@cookpad.com>
2014-06-21 05:35:09 +0900

    ｳｪｰｲ

--- weeeei/src/main/java/me/tmd/weeei/Application.java
+++ weeeei/src/main/java/me/tmd/weeei/Application.java
@@ -1,6 +1,8 @@
 package me.tmd.weeei;
 
 import com.parse.Parse;
+import com.parse.ParseInstallation;
+import com.parse.PushService;
 
 /**
  * Created by takatoshi-maeda on 2014/06/21.
@@ -12,6 +14,8 @@ public class Application extends android.app.Application {
         Parse.initialize(getApplicationContext(),
                 getApplicationContext().getString(R.string.parse_application_id),
                 getApplicationContext().getString(R.string.parse_client_key));
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+        PushService.setDefaultPushCallback(getApplicationContext(), MainActivity.class);
     }
 
 }
--- weeeei/src/main/java/me/tmd/weeei/MainActivity.java
+++ weeeei/src/main/java/me/tmd/weeei/MainActivity.java
@@ -10,7 +10,10 @@ import android.widget.Toast;
 import com.parse.LogInCallback;
 import com.parse.ParseAnonymousUtils;
 import com.parse.ParseException;
+import com.parse.ParseInstallation;
 import com.parse.ParseUser;
+import com.parse.PushService;
+import com.parse.SaveCallback;
 
 public class MainActivity extends Activity {
 
@@ -21,47 +24,57 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        mButtonGo = (Button)findViewById(R.id.btn_go);
-        mEditUserName = (EditText)findViewById(R.id.edit_user_name);
-        if (savedInstanceState == null) {
-            if (ParseUser.getCurrentUser() == null) {
-                changeLoginView();
-            } else {
-                changeMainView();
-            }
-        }
+
+        mButtonGo = (Button) findViewById(R.id.btn_go);
+        mEditUserName = (EditText) findViewById(R.id.edit_user_name);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        if (ParseUser.getCurrentUser() == null) {
-            mButtonGo.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    ParseAnonymousUtils.logIn(new LogInCallback() {
-                        @Override
-                        public void done(ParseUser user, ParseException e) {
-                            if (user != null) {
-                                user.setUsername(mEditUserName.getText().toString());
-                                changeMainView();
-                            } else {
-                                Toast.makeText(MainActivity.this, "ｳｪｰｲ...", Toast.LENGTH_LONG);
-                            }
-                        }
-                    });
-                }
-            });
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null && currentUser.isAuthenticated()) {
+            PushService.subscribe(getApplicationContext(), currentUser.getUsername(), MainActivity.class);
+            changeMainView();
+        } else {
+            changeLoginView();
         }
     }
 
     private void changeLoginView() {
         findViewById(R.id.wrapper_login).setVisibility(View.VISIBLE);
+        findViewById(R.id.wrapper_main).setVisibility(View.GONE);
+
+        mButtonGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseAnonymousUtils.logIn(new LogInCallback() {
+                    @Override
+                    public void done(final ParseUser user, ParseException e) {
+                        if (user != null) {
+                            user.setUsername(mEditUserName.getText().toString());
+                            user.saveInBackground(new SaveCallback() {
+                                @Override
+                                public void done(ParseException e) {
+                                    PushService.subscribe(getApplicationContext(), user.getUsername(), MainActivity.class);
+                                }
+                            });
+                            changeMainView();
+                        } else {
+                            Toast.makeText(MainActivity.this, "ｳｪｰｲ...", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 
     private void changeMainView() {
         findViewById(R.id.wrapper_login).setVisibility(View.GONE);
+        findViewById(R.id.wrapper_main).setVisibility(View.VISIBLE);
     }
 
 }
